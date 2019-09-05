ece70fc52076db9aca6f81ca82702f389acd9a02
Andy Park <andy@bigbearlabs.com>
2019-09-06 00:16:20 +0200

    add a framework target for macOS.

---
+++ ReSwiftMonitorMac/ReSwiftMonitorMac.h
@@ -0,0 +1,19 @@
+//
+//  ReSwiftMonitorMac.h
+//  ReSwiftMonitorMac
+//
+//  Created by ilo on 03/10/2018.
+//  Copyright © 2018 Takuya Ohsawa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReSwiftMonitorMac.
+FOUNDATION_EXPORT double ReSwiftMonitorMacVersionNumber;
+
+//! Project version string for ReSwiftMonitorMac.
+FOUNDATION_EXPORT const unsigned char ReSwiftMonitorMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSwiftMonitorMac/PublicHeader.h>
+
+
